b599467538ed466cac73833f15d3e06a13baf67e
C. Scott Ananian <cscott@cscott.net>
2003-03-26 21:45:46 +0000

    Implement character-index-to-line-and-column translation.

--- src/parser/PFile.java
+++ src/parser/PFile.java
@@ -6,9 +6,12 @@ import net.cscott.gjdoc.SourcePosition;
 import java.io.BufferedReader;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  * The <code>PFile</code> class wraps a <code>java.io.File</code> object
  * and a lazily-created table which allows translation of raw character
@@ -25,12 +28,87 @@ public class PFile {
 	this.file = file; this.encoding = encoding; this.reporter = reporter;
     }
 
+    /** Build the line-starting position and tab-position tables. */
+    private void buildTables() {
+	assert lineStarts==null && tabIndex==null;
+	List<String> lines = new ArrayList<String>();
+	String contents = FileUtil.snarf(file, encoding, reporter).left;
+	IntVector lineStartsVec = new IntVector();
+	IntVector tabIndexVec = new IntVector();
+	Matcher lineMatch = LINES.matcher(contents);
+	while (lineMatch.find())
+	    lineStartsVec.add(lineMatch.start());
+	lineStartsVec.add(contents.length()); // sentinel at end.
+	Matcher tabMatch = TAB.matcher(contents);
+	while (tabMatch.find())
+	    tabIndexVec.add(tabMatch.start());
+	lineStarts = lineStartsVec.toArray();
+	tabIndex = tabIndexVec.toArray();
+	assert lineStarts!=null && tabIndex!=null;
+    }
+    /** sorted list of line starting indices. */
+    transient int[] lineStarts;
+    /** sorted list of tab character positions. */
+    transient int[] tabIndex;
+    /** Pattern which matches a line. */
+    private static final Pattern LINES = Pattern.compile
+	("^.*$", Pattern.MULTILINE);
+    /** Pattern which matches a tab character. */
+    private static final Pattern TAB = Pattern.compile("\\t");
+    /** Helper class to keep track of a growing vector of integers. */
+    private static class IntVector {
+	int[] buf=new int[16]; int size=0;
+	void add(int val) {
+	    if (size==buf.length) resize();
+	    buf[size++]=val;
+	}
+	private void resize() {
+	    int[] nbuf = new int[buf.length*2];
+	    System.arraycopy(buf, 0, nbuf, 0, buf.length);
+	    buf = nbuf;
+	}
+	int[] toArray() {
+	    int[] nbuf = new int[size];
+	    System.arraycopy(buf, 0, nbuf, 0, size);
+	    return nbuf;
+	}
+    }
+    /** Convert the character index to a line and column number. */
     SourcePosition convert(int charIndex) {
-	assert false : "not implemented";
+	if (lineStarts==null) buildTables();
+	// locate start of line.
+	int line = Arrays.binarySearch(lineStarts, charIndex);
+	if (line<0) line=-(1+line)-1;
+	if (line<0 || line>=lineStarts.length-1)
+	    return new SourcePosition() {
+		    public File file() { return file; }
+		    public int line() { return 0; }
+		    public int column() { return 0; }
+		};
+	// now correct for tabs.
+	int startOfLine = lineStarts[line];
+	int firstTab = Arrays.binarySearch(tabIndex, startOfLine);
+	if (firstTab<0) firstTab=-(1+firstTab);
+	int lastTab = Arrays.binarySearch(tabIndex, charIndex);
+	if (lastTab<0) lastTab=-(1+lastTab);
+	int lastPos = startOfLine, column = 0;
+	for (int i=firstTab; i<lastTab; i++) {
+	    int tabPos = tabIndex[i];
+	    column += (tabPos-lastPos);
+	    // now correct for the tab.
+	    column = ((column+8)/8)*8;
+	    // update lastPos
+	    lastPos = tabPos+1;
+	}
+	// adjust column to get to charIndex (no more tabs in between)
+	column += (charIndex-lastPos);
+	// okay, done.
+	final int LINE = line+1; // one-based
+	final int COLUMN = column+1; // one-based
 	return new SourcePosition() {
 		public File file() { return file; }
-		public int line() { return 0; }
-		public int column() { return 0; }
+		public int line() { return LINE; }
+		public int column() { return COLUMN; }
 	    };
     }
     static PFile get(File f, String encoding, DocErrorReporter reporter) {
