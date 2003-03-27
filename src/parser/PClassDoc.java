4a5aad2f9fbd7aecc2e0eac797b9ab4004f095c6
C. Scott Ananian <cscott@cscott.net>
2003-03-27 02:08:59 +0000

    Implement PClassDoc.

--- src/parser/PClassDoc.java
+++ src/parser/PClassDoc.java
@@ -12,6 +12,11 @@ import net.cscott.gjdoc.MethodDoc;
 import net.cscott.gjdoc.PackageDoc;
 import net.cscott.gjdoc.Type;
 
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 /**
  * The <code>PClassDoc</code> interface represents a java class and
@@ -26,24 +31,124 @@ import java.util.List;
  * @author  C. Scott Ananian (cscott@cscott.net)
  * @version $Id$
  */
-abstract class PClassDoc extends PProgramElementDoc
+class PClassDoc extends PProgramElementDoc
     implements net.cscott.gjdoc.ClassDoc {
-    PClassDoc(ParseControl pc) { super(pc); }
-    public abstract ClassType type();
-    public abstract List<ConstructorDoc> constructors();
-    public abstract boolean definesSerializableFields();
-    public abstract List<FieldDoc> fields();
-    public abstract ClassDoc findClass(String className);
-    public abstract List<ClassType> importedClasses();
-    public abstract List<PackageDoc> importedPackages();
-    public abstract List<ClassDoc> innerClasses();
-    public abstract List<Type> interfaces();
-    public abstract boolean isAbstract();
-    public abstract boolean isExternalizable();
-    public abstract boolean isSerializable();
-    public abstract List<MethodDoc> methods();
-    public abstract List<FieldDoc> serializableFields();
-    public abstract List<MethodDoc> serializationMethods();
-    public abstract boolean subclassOf(ClassDoc cd);
-    public abstract Type superclass();
+    final TypeContext typeContext, tagContext;
+    final List<ClassTypeVariable> typeParameters =
+	new ArrayList<ClassTypeVariable>();
+    final String name;
+    final List<ConstructorDoc> constructors = new ArrayList<ConstructorDoc>();
+    boolean definesSerializableFields = false;
+    final List<FieldDoc> fields = new ArrayList<FieldDoc>();
+    final List<ClassDoc> innerClasses = new ArrayList<ClassDoc>();
+    final List<Type> interfaces = new ArrayList<Type>();
+    final List<MethodDoc> methods = new ArrayList<MethodDoc>();
+    Type superclass = null;
+    final String commentText;
+    final PSourcePosition commentPosition;
+
+    PClassDoc(ParseControl pc, PPackageDoc containingPackage,
+	      PCompilationUnit compilationUnit,
+	      PClassDoc containingClass, int modifiers, String name,
+	      String commentText, PSourcePosition commentPosition) {
+	super(pc, containingPackage, containingClass, modifiers);
+	this.name = name;
+	this.typeContext =
+	    new TypeContext(pc, containingPackage, compilationUnit,
+			    this, null);
+	this.tagContext =
+	    new TypeContext(pc, containingPackage, compilationUnit,
+			    containingClass, null);
+	this.commentText = commentText;
+	this.commentPosition = commentPosition;
+    }
+    List<ClassTypeVariable> typeParameters() {
+	return Collections.unmodifiableList(typeParameters);
+    }
+    public ClassType type() {
+	String outerClass = (containingClass()==null) ? "" :
+	    containingClass().name()+".";
+	return new PEagerClassType(pc, containingPackage().name(),
+				   outerClass+name(), typeParameters());
+    }
+    public List<ConstructorDoc> constructors() {
+	return Collections.unmodifiableList(constructors);
+    }
+    public boolean definesSerializableFields() {
+	return definesSerializableFields;
+    }
+    public List<FieldDoc> fields() {
+	return Collections.unmodifiableList(fields);
+    }
+    public final ClassDoc findClass(String className) {
+	return ((ClassType) typeContext.lookupTypeName(className))//xxx
+	    .asClassDoc();
+    }
+    public final List<ClassType> importedClasses() {
+	ArrayList<ClassType> result = new ArrayList<ClassType>();
+	for (Iterator<String> it=typeContext.compilationUnit.singleTypeImport
+		 .iterator(); it.hasNext(); )
+	    result.add((ClassType)typeContext.lookupTypeName(it.next()));//xxx
+	result.trimToSize();
+	return Collections.unmodifiableList(result);
+    }
+    public final List<PackageDoc> importedPackages() {
+	return Collections.unmodifiableList(new ArrayList<PackageDoc>(typeContext.compilationUnit.onDemandImport));
+    }
+    public List<ClassDoc> innerClasses() {
+	return Collections.unmodifiableList(innerClasses);
+    }
+    public List<Type> interfaces() {
+	return Collections.unmodifiableList(interfaces);
+    }
+    public final boolean isAbstract() {
+	return Modifier.isAbstract(modifierSpecifier());
+    }
+    public final boolean isExternalizable() {
+	return instanceOf(new PEagerClassType
+	    (pc, "java.io", "Externalizable",
+	     Arrays.asList(new ClassTypeVariable[0])));
+    }
+    public final boolean isSerializable() {
+	return instanceOf(new PEagerClassType
+	    (pc, "java.io", "Serializable",
+	     Arrays.asList(new ClassTypeVariable[0])));
+    }
+
+    public List<MethodDoc> methods() {
+	return Collections.unmodifiableList(methods);
+    }
+    public List<FieldDoc> serializableFields() {
+	throw new RuntimeException("unimplemented");
+    }
+    public List<MethodDoc> serializationMethods() {
+	throw new RuntimeException("unimplemented");
+    }
+    public boolean subclassOf(ClassDoc cd) {
+	throw new RuntimeException("unimplemented");
+    }
+    public Type superclass() { return superclass; }
+    public boolean instanceOf(Type t) {
+	throw new RuntimeException("unimplemented");
+    }
+    // methods abstract in PProgramElementDoc
+    public String qualifiedName() {
+	StringBuffer sb = new StringBuffer();
+	if (containingClass()!=null) {
+	    sb.append(containingClass().qualifiedName());
+	    sb.append('.');
+	} else if (containingPackage().name().length()>0) {
+	    sb.append(containingPackage().name());
+	    sb.append('.');
+	}
+	sb.append(name());
+	return sb.toString();
+    }
+    public String toString() { return qualifiedName(); }
+    // methods abstract in PDoc
+    public String getRawCommentText() { return commentText; }
+    public PSourcePosition getRawCommentPosition() { return commentPosition; }
+    public TypeContext getCommentContext() { return tagContext; }
+    public boolean isIncluded() { return true; }
+    public String name() { return name; }
 }
